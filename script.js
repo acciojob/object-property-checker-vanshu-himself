2a3e42989eb396daf1939cb1e391db1c8835365a
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-04-15 19:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	for(let i in sampleObject){
+if(i == key) return true;
+		
+	} return false;
 }
 
 // Do not change the code below
